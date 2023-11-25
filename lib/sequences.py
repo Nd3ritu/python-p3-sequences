5d6c6559bee80239bdc5eacb263f97322f5a5ea2
Matthew <+>
2023-11-25 08:42:38 +0300

    completed sequence lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print("[]")
+        return 
+    
+    elif length == 1:
+        print("[0]")
+        return
+    
+    seq = [0,1]
+
+    while len(seq) < length:
+        next = seq[-1] + seq[-2]
+        seq.append(next)
+
+    print(seq)
+
+print_fibonacci(0)
